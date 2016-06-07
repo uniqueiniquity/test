7a29aa5843fbce727bb96efbecf96744efcc449f
Benjamin Lichtman <runite623@gmail.com>
2016-06-07 10:24:25 -0700

    update

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -2,31 +2,35 @@
 //
 
 #include "stdafx.h"
-class Demo {
-public:
-	struct product {
-		int weight;
-		double price;
-	} apple;
+namespace TestFramework {
+	class Demo {
+	public:
+		struct product {
+			int weight;
+			double price;
+		} apple;
 
-	int foo()
-	{
-		apple.weight = 5;
-		apple.price = 42.4;
-		//printf("hello world!");
-		return apple.weight;
-	}
+		int foo()
+		{
+			apple.weight = 5;
+			apple.price = 42.4;
+			//printf("hello world!");
+			return apple.weight;
+		}
 
-public:
-	Demo() {
-		apple.weight = 15;
-		apple.price = 52.5;
-		foo();
-	}
+	public:
+		Demo() {
+			apple.weight = 15;
+			apple.price = 52.5;
+			foo();
+		}
 
-};
+	};
+}
 
 int main()
 {
-	Demo* d = new Demo();
+	TestFramework::Demo* d = new TestFramework::Demo();
+
+	return 0;
 }
