cb40b7dd4e798169a0de14e80670d4e904fedbeb
Benjamin Lichtman <runite623@gmail.com>
2016-07-12 14:03:05 -0700

    added typescript demo file

---
+++ test.ts
@@ -0,0 +1,15 @@
+/** 
+ * Documentation for C 
+ */
+class C { 
+    /** 
+     * constructor documentation
+     * @param a my parameter documentation
+     * @param b another parameter documentation
+     */
+    constructor(a: string, b: C) { }
+
+    foo(bar:string) {
+        return "baz";
+    }
+}
